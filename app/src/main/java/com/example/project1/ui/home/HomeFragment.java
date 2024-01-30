a28bbaeb8fe740692d4a46a954441a9d5c788609
AndreGrossberg <71469970+AndreGrossberg@users.noreply.github.com>
2024-01-30 00:03:56 -0500

    tried to implement event listener and added assignment class update Assignment.java update fragment_home.xml update HomeFragment.java update HomeViewModel.java

--- app/src/main/java/com/example/project1/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/project1/ui/home/HomeFragment.java
@@ -5,16 +5,37 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.fragment.NavHostFragment;
 
+import com.example.project1.R;
 import com.example.project1.databinding.FragmentHomeBinding;
+import com.google.android.material.textfield.TextInputLayout;
 
 public class HomeFragment extends Fragment {
 
     private FragmentHomeBinding binding;
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        view.findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                TextInputLayout textInputLayout = view.findViewById(R.id.input);
+//                if (textInputLayout != null && textInputLayout.getEditText() != null) {
+//                    String text = String.valueOf(textInputLayout.getEditText().getText());
+//                    // Rest of your code
+//                }
+
+                Toast myToast = Toast.makeText(getActivity(), "Hellow world", Toast.LENGTH_SHORT);
+                myToast.show();
+            }
+        });
+    }
+
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -25,7 +46,10 @@ public class HomeFragment extends Fragment {
         View root = binding.getRoot();
 
         final TextView textView = binding.textHome;
+        final TextView textView1 = binding.textView2;
         homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        homeViewModel.getText2().observe(getViewLifecycleOwner(), textView1::setText);
+
         return root;
     }
 
@@ -34,4 +58,5 @@ public class HomeFragment extends Fragment {
         super.onDestroyView();
         binding = null;
     }
+
 }
--- app/src/main/java/com/example/project1/ui/home/HomeViewModel.java
+++ app/src/main/java/com/example/project1/ui/home/HomeViewModel.java
@@ -7,13 +7,22 @@ import androidx.lifecycle.ViewModel;
 public class HomeViewModel extends ViewModel {
 
     private final MutableLiveData<String> mText;
+    private final MutableLiveData<String> mText2;
+
 
     public HomeViewModel() {
         mText = new MutableLiveData<>();
         mText.setValue("This is home fragment");
+
+        mText2 = new MutableLiveData<>();
+        mText2.setValue("Bruh");
     }
 
     public LiveData<String> getText() {
         return mText;
     }
+    public LiveData<String> getText2() {
+        return mText2;
+    }
+
 }
