af745c8847b84a11b5b9c1fc6e3489f6d2b2d5f2
Michael Vega <michaelvega8888@gmail.com>
2024-01-30 19:52:47 -0500

    bad button update fragment_home.xml update HomeFragment.java update HomeViewModel.java

--- app/src/main/java/com/example/project1/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/project1/ui/home/HomeFragment.java
@@ -10,45 +10,46 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
-import androidx.navigation.fragment.NavHostFragment;
 
-import com.example.project1.R;
 import com.example.project1.databinding.FragmentHomeBinding;
 import com.google.android.material.textfield.TextInputLayout;
+
+import com.example.project1.R;
+import java.util.ArrayList;
 
 public class HomeFragment extends Fragment {
 
     private FragmentHomeBinding binding;
+    private ArrayList<String> inputList = new ArrayList<>();
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        HomeViewModel homeViewModel = new ViewModelProvider(this).get(HomeViewModel.class);
+
+        // Update the TextView with the list of inputs
+        final TextView inputListTextView = binding.inputListTextView; // assuming this is the ID of your TextView
+        homeViewModel.getInputListText().observe(getViewLifecycleOwner(), inputListTextView::setText);
+
         view.findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-//                TextInputLayout textInputLayout = view.findViewById(R.id.input);
-//                if (textInputLayout != null && textInputLayout.getEditText() != null) {
-//                    String text = String.valueOf(textInputLayout.getEditText().getText());
-//                    // Rest of your code
-//                }
-
-                Toast myToast = Toast.makeText(getActivity(), "Hellow world", Toast.LENGTH_SHORT);
-                myToast.show();
+                TextInputLayout textInputLayout = view.findViewById(R.id.input);
+                if (textInputLayout != null && textInputLayout.getEditText() != null) {
+                    String text = textInputLayout.getEditText().getText().toString();
+                    homeViewModel.addInput(text);
+                    Toast.makeText(getActivity(), "Input saved", Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
 
-
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        HomeViewModel homeViewModel =
-                new ViewModelProvider(this).get(HomeViewModel.class);
-
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        HomeViewModel homeViewModel = new ViewModelProvider(this).get(HomeViewModel.class);
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textHome;
-        final TextView textView1 = binding.textView2;
-        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
-        homeViewModel.getText2().observe(getViewLifecycleOwner(), textView1::setText);
+        // Rest of your onCreateView code
 
         return root;
     }
@@ -58,5 +59,4 @@ public class HomeFragment extends Fragment {
         super.onDestroyView();
         binding = null;
     }
-
 }
--- app/src/main/java/com/example/project1/ui/home/HomeViewModel.java
+++ app/src/main/java/com/example/project1/ui/home/HomeViewModel.java
@@ -4,11 +4,13 @@ import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import java.util.List;
+
 public class HomeViewModel extends ViewModel {
 
     private final MutableLiveData<String> mText;
     private final MutableLiveData<String> mText2;
-
+    private final MutableLiveData<String> inputListText;
 
     public HomeViewModel() {
         mText = new MutableLiveData<>();
@@ -16,13 +18,26 @@ public class HomeViewModel extends ViewModel {
 
         mText2 = new MutableLiveData<>();
         mText2.setValue("Bruh");
+
+        inputListText = new MutableLiveData<>();
+        inputListText.setValue("");
     }
 
     public LiveData<String> getText() {
         return mText;
     }
+
     public LiveData<String> getText2() {
         return mText2;
     }
 
+    public LiveData<String> getInputListText() {
+        return inputListText;
+    }
+
+    public void addInput(String input) {
+        String currentText = inputListText.getValue();
+        if (currentText == null) currentText = "";
+        inputListText.setValue(currentText + "\n" + input);
+    }
 }
