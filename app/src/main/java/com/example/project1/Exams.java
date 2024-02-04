36422809a3bcfa1dd311301e5fd1fc390a990b7d
AndreGrossberg <71469970+AndreGrossberg@users.noreply.github.com>
2024-02-03 20:00:59 -0500

    update Exams.java

---
+++ app/src/main/java/com/example/project1/Exams.java
@@ -0,0 +1,43 @@
+package com.example.project1;
+
+import java.util.Date;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Exams {
+    String title;
+    String time;
+    String location;
+    String details;
+
+    public static ArrayList<Exams> exams = new ArrayList<>();
+
+    public Exams(String title, String time, String location, String details) {
+        this.title = title;
+        this.time = time;
+        this.location = location;
+        this.details = details;
+
+        exams.add(this);
+
+    }
+
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) return false;
+        if (obj == this) return true;
+        if (!(obj instanceof Exams)) return false;
+        Exams o = (Exams) obj;
+        return o.title == this.title;
+    }
+
+    public void removeAssignment() {
+        if (exams.size() == 0) {
+            return;
+        }
+        exams.remove(this);
+    }
+}
+
+
