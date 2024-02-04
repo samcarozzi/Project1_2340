c8f5f92ab9d1d6e11d8c591af0d17a728c5cb429
samcarozzi <100541583+samcarozzi@users.noreply.github.com>
2024-02-03 19:03:54 -0500

    Renamed method, added comments. See line 45.

--- app/src/main/java/com/example/project1/Assignment.java
+++ app/src/main/java/com/example/project1/Assignment.java
@@ -1,5 +1,52 @@
 package com.example.project1;
 
-public class Assignment {
+import java.util.Date;
 
+public class Assignment {
+    private String title;
+    private Date dueDate;
+    private String associatedClass; // This could be a course name or an identifier
+
+    // Constructor
+    public Assignment(String title, Date dueDate, String associatedClass) {
+        this.title = title;
+        this.dueDate = dueDate;
+        this.associatedClass = associatedClass;
+    }
+
+    // Getters and Setters
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Date getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(Date dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public String getAssociatedClass() {
+        return associatedClass;
+    }
+
+    public void setAssociatedClass(String associatedClass) {
+        this.associatedClass = associatedClass;
+    }
+
+    // Optionally, override toString() for easy printing/debugging
+    @Override
+    public String toString() {
+        return "Assignment{" +
+                "title='" + title + '\'' +
+                ", dueDate=" + dueDate +
+                ", associatedClass='" + associatedClass + '\'' +
+                '}';
+    }
 }
+
