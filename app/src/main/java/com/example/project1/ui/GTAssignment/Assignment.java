cb86f1b2b725d54680f843bd8d7cc7f469178cae
Michael Vega <michaelvega8888@gmail.com>
2024-02-03 19:43:04 -0500

    move Assignment.java

--- app/src/main/java/com/example/project1/ui/GTAssignment/Assignment.java
+++ app/src/main/java/com/example/project1/ui/GTAssignment/Assignment.java
@@ -1,9 +1,8 @@
-package com.example.project1;
+package com.example.project1.ui.GTAssignment;
 
 import java.util.Date;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Comparator;
 
 public class Assignment {
     private String title;
